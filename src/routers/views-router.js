caea6383111ca5f4dd92a841c829b367d2930af3
eunji <yhn01130.n@gmail.com>
2022-05-31 18:22:29 +0900

    REFACTOR : view-router 충돌해결, 경로 연결

--- src/routers/views-router.js
+++ src/routers/views-router.js
@@ -6,18 +6,13 @@ const viewsRouter = express.Router();
 // 페이지별로 html, css, js 파일들을 라우팅함
 // 아래와 같이 하면, http://localhost:5000/ 에서는 views/home/home.html 파일을,
 // http://localhost:5000/register 에서는 views/register/register.html 파일을 화면에 띄움
-<<<<<<< HEAD
+
 viewsRouter.use('/', serveStatic('home'));
 viewsRouter.use('/register', serveStatic('register'));
 viewsRouter.use('/login', serveStatic('login'));
+viewsRouter.use("/userInfo/changeUserInfo", serveStatic("changeUserInfo"));
 viewsRouter.use('/userInfo', serveStatic('userInfo'));
 
-=======
-viewsRouter.use("/", serveStatic("home"));
-viewsRouter.use("/register", serveStatic("register"));
-viewsRouter.use("/login", serveStatic("login"));
-viewsRouter.use("/userInfo/changeUserInfo", serveStatic("changeUserInfo"));
->>>>>>> 619eafd142ba1b8714ad1fe0ed458dc75c5bb274
 // views 폴더의 최상단 파일인 rabbit.png, api.js 등을 쓸 수 있게 함
 viewsRouter.use("/", serveStatic(""));
 
