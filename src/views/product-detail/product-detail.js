0d24612a4bace21820a90339f6f98c24ccebe1eb
jhoon9494 <wjdgns9406@gmail.com>
2022-06-02 14:37:14 +0900

    Feat: 상품 상세 페이지에서 주문하기 및 장바구니 페이지로 전달기능 구현

--- src/views/product-detail/product-detail.js
+++ src/views/product-detail/product-detail.js
@@ -15,7 +15,36 @@ async function insertTextToLanding() {
   const productId = getProductId();
   const getData = await getDataFromApi(productId);
   createProductDetail(getData).forEach((el) => (containerDiv.innerHTML += el));
-  // console.log(productId);
+
+  const orderBtn = document.querySelector("#orderBtn");
+  const putCartBtn = document.querySelector("#putCartBtn");
+  const productCount = document.querySelector("#productCount");
+  // 주문정보 넘겨주기
+  orderBtn.addEventListener("click", () => {
+    const data = {
+      count: Number(productCount.value),
+      name: getData[0].productName,
+      price: getData[0].productPrice,
+    };
+    sessionStorage.setItem("product", JSON.stringify(data));
+    window.location.href = "/order";
+  });
+
+  //장바구니 넘겨주기
+  putCartBtn.addEventListener("click", () => {
+    const productName = getData[0].productName;
+    const prevData = JSON.parse(sessionStorage.getItem(productName));
+    const data = {
+      count: prevData
+        ? Number(prevData.count) + Number(productCount.value)
+        : Number(productCount.value),
+      name: productName,
+      price: getData[0].productPrice,
+    };
+
+    sessionStorage.setItem(productName, JSON.stringify(data));
+    window.location.href = "/cart";
+  });
 }
 
 // api를 통해 상품 상세 정보를 받아온 후 html에 표시
@@ -33,9 +62,12 @@ function createProductDetail(data) {
         <p>${el.productContent}</p>
         <p>${el.productPrice.toLocaleString()}원</p>
         </div>
+        <div>
+          <input id="productCount" type="number" value="1" min="1">
+        </div>
         <div class="btnBox">
-          <button>주문하기</button>
-          <button>장바구니 담기</button>
+          <button id="orderBtn">주문하기</button>
+          <button id="putCartBtn">장바구니 담기</button>
         </div>
       </div>`
   );
