859c646be73c8b3d3e1f2b41e0ca702789c3f0c4
eunji <yhn01130.n@gmail.com>
2022-06-02 14:06:22 +0900

    REFACTOR : 카트 경로 연결

--- src/routers/views-router.js
+++ src/routers/views-router.js
@@ -21,7 +21,7 @@ viewsRouter.use("/product/product-detail", serveStatic("product-detail"));
 viewsRouter.use("/orderComplete", serveStatic("orderComplete"));
 viewsRouter.use("/order", serveStatic("order"));
 
-viewsRouter.use("cart", serveStatic("cart"));
+viewsRouter.use("/cart", serveStatic("cart"));
 // views 폴더의 최상단 파일인 rabbit.png, api.js 등을 쓸 수 있게 함
 viewsRouter.use("/", serveStatic(""));
 
