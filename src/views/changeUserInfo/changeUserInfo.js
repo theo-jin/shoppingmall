152c190902021874f80e5407510d89cefa807b8f
jhoon9494 <wjdgns9406@gmail.com>
2022-06-02 18:47:53 +0900

    Refactor: changeNavbar 함수의 잦은 반복으로 리팩토링 실시

--- src/views/changeUserInfo/changeUserInfo.js
+++ src/views/changeUserInfo/changeUserInfo.js
@@ -1,5 +1,6 @@
 import * as Api from "/api.js";
 import { validatePhoneNumber } from "/useful-functions.js";
+import { changeNavbar } from "/changeNavbar.js";
 
 // 요소(element), input 혹은 상수
 const submitButton = document.querySelector("#submitButton");
@@ -19,6 +20,7 @@ addAllEvents();
 // html에 요소를 추가하는 함수들을 묶어주어서 코드를 깔끔하게 하는 역할임.
 async function addAllElements() {
   await getDataFromApi();
+  changeNavbar();
 }
 
 // 여러 개의 addEventListener들을 묶어주어서 코드를 깔끔하게 하는 역할임.
