e809c4ee7ab0180c43f7fcb0c333d540798ae156
jhoon94 <wjdgns9406@gmail.com>
2022-06-02 13:48:55 +0900

    Refactor: views-router 변경 및 유저삭제 구현 완료

--- src/views/userSignOut/usersignOut.js
+++ src/views/userSignOut/usersignOut.js
@@ -1,28 +1,25 @@
-import * as Api from '/api.js';
+import * as Api from "/api.js";
 
-const passwordInput = document.getElementById('passwordInput');
-const submitButton = document.getElementById('submitButton');
+const passwordInput = document.getElementById("passwordInput");
+const submitButton = document.getElementById("submitButton");
 
 async function userSignOut(e) {
   e.preventDefault();
-  if (!confirm('정말 회원 탈퇴하시겠습니까?')) return;
+  if (!confirm("정말 회원 탈퇴하시겠습니까?")) return;
   const password = passwordInput.value;
-  const data = { email,password };
+  const data = { email, password };
 
   // TODO 로그아웃 API요청
   // 로그아웃 api 요청
   try {
     //TODO 어디서 받아와야할까요?delete로 안되는거같던데
-    const res = await Api.(data);
+    const res = await Api.delete(data);
     alert(`회원탈퇴 완료하였습니다.감사합니다.${res.message}`);
-   
-    //회원탈퇴 성공, 토큰을 세션 스토리지에 저장
-    sessionStorage.removeItem('token');
-    //홈으로 이동. 
-    window.location.href = '/';
+
+    //홈으로 이동.
+    window.location.href = "/";
   } catch (err) {
     alert(`문제가 발생하였습니다. 비밀번호를 다시확인해주세요${err.message}`);
   }
 }
-submitButton.addEventListener('click', userSignOut);
-
+submitButton.addEventListener("click", userSignOut);
