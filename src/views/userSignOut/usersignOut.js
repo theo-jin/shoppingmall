6e8b0f42f78439a7858ac8330eeb84db8a55a833
jhoon9494 <wjdgns9406@gmail.com>
2022-06-02 20:18:47 +0900

    Fix: 회원탈퇴 기능 수정

--- src/views/userSignOut/usersignOut.js
+++ src/views/userSignOut/usersignOut.js
@@ -1,19 +1,15 @@
 import * as Api from "/api.js";
 
-const passwordInput = document.getElementById("passwordInput");
 const submitButton = document.getElementById("submitButton");
 
 async function userSignOut(e) {
   e.preventDefault();
   if (!confirm("정말 회원 탈퇴하시겠습니까?")) return;
-  const password = passwordInput.value;
-  const data = { email, password };
 
-  // TODO 로그아웃 API요청
   // 로그아웃 api 요청
   try {
-    //TODO 어디서 받아와야할까요?delete로 안되는거같던데
-    const res = await Api.delete(data);
+    const res = await Api.delete("/api/user");
+    sessionStorage.removeItem("token");
     alert(`회원탈퇴 완료하였습니다.감사합니다.${res.message}`);
 
     //홈으로 이동.
