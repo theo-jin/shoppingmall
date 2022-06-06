4fe89c15de4e0bdb2480267ce5211e8dfccd0af7
jhoon9494 <wjdgns9406@gmail.com>
2022-06-06 22:04:55 +0900

    Refactor: 코드 리팩토링
    1. api token 얻는 코드 삭제
    2. user api 경로 변경으로 관련된 코드 수정

--- src/views/userSignOut/usersignOut.js
+++ src/views/userSignOut/usersignOut.js
@@ -2,20 +2,24 @@ import * as Api from "/api.js";
 
 const submitButton = document.getElementById("submitButton");
 
+// 쿠키 삭제 함수
+function deleteCookie(name) {
+  document.cookie = name + "=; expires=Thu, 01 Jan 1999 00:00:10 GMT;domain=localhost; path=/";
+}
+
 async function userSignOut(e) {
   e.preventDefault();
   if (!confirm("정말 회원 탈퇴하시겠습니까?")) return;
 
-  // 로그아웃 api 요청
+  // 회원 정보 삭제 api 요청
   try {
-    const res = await Api.delete("/api/user");
-    sessionStorage.removeItem("token");
-    alert(`회원탈퇴 완료하였습니다.감사합니다.${res.message}`);
-
+    const res = await Api.delete("/api/");
+    alert(`회원탈퇴 완료하였습니다.감사합니다.`);
+    deleteCookie("token");
     //홈으로 이동.
     window.location.href = "/";
   } catch (err) {
-    alert(`문제가 발생하였습니다. 비밀번호를 다시확인해주세요${err.message}`);
+    alert(`문제가 발생하였습니다. 비밀번호를 다시확인해주세요.`);
   }
 }
 submitButton.addEventListener("click", userSignOut);
